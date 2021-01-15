faaed194864bff1d40971d0daeebd3c8f78d1f18
JowettC <jowettchng@outlook.com>
2021-01-15 12:56:09 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ProductZoomer from 'vue-product-zoomer'
 
 Vue.config.productionTip = false
+Vue.use(ProductZoomer)
 
 new Vue({
   render: h => h(App),
