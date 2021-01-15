a8ab82f4fcb083af6d5db32f7383a7a4c8bc3e5a
JowettC <jowettchng@outlook.com>
2021-01-15 12:56:00 +0800

    package

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,11 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>{{msg}}</h1>
+    <ProductZoomer
+  :base-images="images"
+  :base-zoomer-options="zoomerOptions"
+/>
+<div>end</div>
   </div>
 </template>
 
@@ -35,6 +14,34 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      'images': {
+          'normal_size': [
+            {
+              'id': 1,
+              'url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTML0bfoxOtyMJjuqDCc_H1iV0XjNXpGcVzow&usqp=CAU'
+            },
+            {
+              'id': 2,
+              'url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTML0bfoxOtyMJjuqDCc_H1iV0XjNXpGcVzow&usqp=CAU'
+            }
+          ],
+      },
+      'zoomerOptions': {
+        zoomFactor: 3, // scale for zoomer
+        pane: 'pane', // three type of pane ['pane', 'container-round', 'container']
+        hoverDelay: 300, // how long after the zoomer take effect
+        namespace: 'zoomer', // add a namespace for zoomer component, useful when on page have mutiple zoomer
+        move_by_click:false, // move image by click thumb image or by mouseover
+        scroll_items: 5, // thumbs for scroll
+        choosed_thumb_border_color: "#bbdefb", // choosed thumb border color
+        scroller_button_style: "line",
+        scroller_position: "left",
+        zoomer_pane_position: "right"
+      }
+    }
   }
 }
 </script>
